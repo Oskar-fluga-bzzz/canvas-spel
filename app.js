94909dd05bc9981f642e342c5efcd552168b613f
Fluga bzzz <116791678+Oskar-fluga-bzzz@users.noreply.github.com>
2023-04-11 19:04:33 +0200

    0.03

--- app.js
+++ app.js
@@ -9,14 +9,20 @@ const c = canvas.getContext("2d")
 c.fillStyle = "#FF0000"
 const midX = canvas.width / 2
 const midY = canvas.height / 2
+var paddle_x = midX
+var paddle_y = midY
+
+// --- knapptryck --- //
+let rightPressed = false;
+let leftPressed = false;
 
 
 // --- spawnar spelaren --- //
 function spawnPlayer() {
     c.beginPath()
-    c.moveTo(midX, midY + 200) // midX , midY
-    c.lineTo(midX - 30, midY + 300) // midX - 30 , midY + 50
-    c.lineTo(midX + 30, midY + 300) // midX + 60 , midY + 50
+    c.moveTo(paddle_x, paddle_y + 200) // midX , midY
+    c.lineTo(paddle_x - 30, paddle_y + 300) // midX - 30 , midY + 50
+    c.lineTo(paddle_x + 30, paddle_y + 300) // midX + 60 , midY + 50
     c.closePath()
 
     c.lineWidth = 10
@@ -26,4 +32,40 @@ function spawnPlayer() {
     c.fillStyle = "white"
     c.fill()
 }
-spawnPlayer()
+
+// --- draw funktion --- //
+function draw() {
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    spawnPlayer()
+    if (rightPressed) { // if sats som kollar efter knapptryck
+        paddle_x = Math.min(paddle_x + 7, canvas.width - 30);
+      } else if (leftPressed) {
+        paddle_x = Math.max(paddle_x - 7, 0);
+      }
+}
+
+draw()
+
+// --- mera knapptryck --- //
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+      rightPressed = true;
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+      leftPressed = true;
+    }
+  }
+  
+  function keyUpHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+      rightPressed = false;
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+      leftPressed = false;
+    }
+  }
+
+
+// --- intervall --- //
+setInterval(draw, 10);
