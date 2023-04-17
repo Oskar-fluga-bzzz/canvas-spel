39b58abcd6673dd1b35ee67ccf6b83180cb9a66a
Oskar-fluga-bzzz <oskar.hughes@skola.taby.se>
2023-04-17 09:40:09 +0200

    0.04

--- app.js
+++ app.js
@@ -12,17 +12,48 @@ const midY = canvas.height / 2
 var paddle_x = midX
 var paddle_y = midY
 
+
 // --- knapptryck --- //
+let upPressed = false;
+let downPressed = false;
 let rightPressed = false;
 let leftPressed = false;
+
+// --- mera knapptryck --- //
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if (e.key === "Up" || e.key === "ArrowUp") {
+      upPressed = true;
+    } else if (e.key === "Down" || e.key === "ArrowDown") {
+      downPressed = true;
+    } else if (e.key === "Right" || e.key === "ArrowRight") {
+      rightPressed = true;
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+      leftPressed = true;
+    }
+  }
+
+  function keyUpHandler(e) {
+    if (e.key === "Up" || e.key === "ArrowUp") {
+      upPressed = false;
+    } else if (e.key === "Down" || e.key === "ArrowDown") {
+      downPressed = false;
+    } else if (e.key === "Right" || e.key === "ArrowRight") {
+      rightPressed = false;
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+      leftPressed = false;
+    }
+  }
 
 
 // --- spawnar spelaren --- //
 function spawnPlayer() {
     c.beginPath()
-    c.moveTo(paddle_x, paddle_y + 200) // midX , midY
-    c.lineTo(paddle_x - 30, paddle_y + 300) // midX - 30 , midY + 50
-    c.lineTo(paddle_x + 30, paddle_y + 300) // midX + 60 , midY + 50
+    c.moveTo(paddle_x - 400, paddle_y) // midX , midY
+    c.lineTo(paddle_x - 450, paddle_y + 20) // midX - 30 , midY + 50
+    c.lineTo(paddle_x - 450, paddle_y - 20) // midX + 60 , midY + 50
     c.closePath()
 
     c.lineWidth = 10
@@ -33,39 +64,31 @@ function spawnPlayer() {
     c.fill()
 }
 
+
+function projectileSpawn(){
+  c.beginPath()
+  c.arc(240, 160, 20, 0, math.PI * 2, false)
+  c.fillStyle = "red"
+  c.fill()
+  c.closePath()
+}
+
 // --- draw funktion --- //
 function draw() {
     c.clearRect(0, 0, canvas.width, canvas.height);
     spawnPlayer()
-    if (rightPressed) { // if sats som kollar efter knapptryck
-        paddle_x = Math.min(paddle_x + 7, canvas.width - 30);
+    if (upPressed) { // if sats som kollar efter knapptryck
+        paddle_y = Math.max(paddle_y - 7, 30);
+      } else if (downPressed) {
+        paddle_y = Math.min(paddle_y + 7, canvas.height - 30);
+      } else if (rightPressed) {
+        paddle_x = Math.min(paddle_x + 7, canvas.width + 375);
       } else if (leftPressed) {
-        paddle_x = Math.max(paddle_x - 7, 0);
+        paddle_x = Math.max(paddle_x - 7, 450);
       }
 }
 
 draw()
-
-// --- mera knapptryck --- //
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-
-function keyDownHandler(e) {
-    if (e.key === "Right" || e.key === "ArrowRight") {
-      rightPressed = true;
-    } else if (e.key === "Left" || e.key === "ArrowLeft") {
-      leftPressed = true;
-    }
-  }
-  
-  function keyUpHandler(e) {
-    if (e.key === "Right" || e.key === "ArrowRight") {
-      rightPressed = false;
-    } else if (e.key === "Left" || e.key === "ArrowLeft") {
-      leftPressed = false;
-    }
-  }
-
 
 // --- intervall --- //
 setInterval(draw, 10);
